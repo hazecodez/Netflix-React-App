c82b873ec96f6df8bc14f02de3620909b0dc16f2
hazecodez <muhammedhashir298@gmail.com>
2023-12-29 20:30:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <>Netflix App coming soon...</>;
+  }
+}
+
+export default App;
